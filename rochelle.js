a415e1dd046319f9cc910c4ab45a5108bf7360a5
Bryan Chug <bryan.chug@aestrea.com>
2012-07-12 03:08:21 +0800

    Updated style

--- rochelle.js
+++ rochelle.js
@@ -2,7 +2,7 @@ Contents = new Meteor.Collection("contents");
 
 function getValue( content ){
 	var object = Contents.findOne({id: content});
-        return object ? object.value : "";
+        return object ? object.value : null;
 }
 
 function setValue( content, value ){
@@ -16,14 +16,16 @@ function setValue( content, value ){
 
 if (Meteor.is_client) {
 	Template.content.title = function () {
-		return getValue("title") || "Rochelle @ 18";
+		var value = getValue("title");
+		return value != null ? value : "Rochelle @ 18";
 	};
 	Template.content.subtitle = function() {
-		return getValue("subtitle") || "PEARLMONT INN ★ JULY 14, 2012";
+		var value = getValue("subtitle");
+		return value != null ? value : "PEARLMONT INN ★ JULY 14, 2012";
 	};
 	Template.content.events = {
 		'blur input': function(e){
-			var content = e.target.id;
+			var content = $(e.target).parent().attr('id');
 			var value = e.target.value;
 			setValue( content, value );
 		}
